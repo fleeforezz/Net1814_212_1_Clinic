485f576c18b4e740dbc669378c0e62a22603dfbc
Trương Minh Nhật <fleeforezz@gmail.com>
2024-06-06 15:52:00 +0700

    Upload src code

---
+++ Source/Net1814_212_1_Clinic/Clinic.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Clinic.WpfApp.UI;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clinic.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_wAppointmentDetail_Click(object sender, RoutedEventArgs e)
+        {
+            var p = new wAppointmentDetail();
+            p.Owner = this;
+            p.Show();
+        }
+
+        private void MenuItem_DpiChanged(object sender, DpiChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Source/Net1814_212_1_Clinic/Clinic.WpfApp/UI/wAppointmentDetail.xaml.cs
@@ -0,0 +1,204 @@
+﻿using Clinic.Business;
+using Clinic.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clinic.WpfApp.UI
+{
+    /// <summary>
+    /// Interaction logic for wAppointmentDetail.xaml
+    /// </summary>
+    public partial class wAppointmentDetail : Window
+    {
+        private readonly IAppointmentDetailBusiness _appointmentDetailBusiness;
+        public wAppointmentDetail()
+        {
+            InitializeComponent();
+            _appointmentDetailBusiness = new AppointmentDetailBusiness();
+            GetAllData();
+        }
+
+        private async void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int appointmentDetailId = Int32.Parse(AppointmentDetailId.Text);
+                var item = await _appointmentDetailBusiness.GetById(appointmentDetailId);
+
+                if (item.Data == null)
+                {
+                    var AppointmentDetail = new AppointmentDetail()
+                    {
+                        AppointmentDetailId = Int32.Parse(AppointmentDetailId.Text),
+                        AppointmentId = Int32.Parse(AppointmentId.Text),
+                        ServiceId = Int32.Parse(ServiceId.Text),
+                        IsPeriodic = Boolean.Parse(isPeriodic.Text),
+                        Day = Int32.Parse(Day.Text),
+                        Month = Int32.Parse(Month.Text),
+                        Year = Int32.Parse(Year.Text),
+                    };
+
+                    var result = await _appointmentDetailBusiness.Save(AppointmentDetail);
+                    MessageBox.Show(result.Message, "Save");
+
+                } 
+                else
+                {
+                    var appointmentDetail = item.Data as AppointmentDetail;
+                    //currency.CurrencyCode = txtCurrencyCode.Text;
+
+
+                    appointmentDetail.AppointmentDetailId = Int32.Parse(AppointmentDetailId.Text);
+                    appointmentDetail.AppointmentId = Int32.Parse(AppointmentId.Text);
+                    appointmentDetail.ServiceId = Int32.Parse(ServiceId.Text);
+                    appointmentDetail.Day = Int32.Parse(Day.Text);
+                    appointmentDetail.Month = Int32.Parse(Month.Text);
+                    appointmentDetail.Year = Int32.Parse(Year.Text);
+
+                    var result = await _appointmentDetailBusiness.Update(appointmentDetail);
+                    MessageBox.Show(result.Message, "Save");
+                }
+
+                AppointmentDetailId.Text = string.Empty;
+                AppointmentId.Text = string.Empty;
+                ServiceId.Text = string.Empty;
+                isPeriodic.Text = string.Empty;
+                Day.Text = string.Empty;
+                Month.Text = string.Empty;
+                Year.Text = string.Empty;
+                GetAllData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+
+        }
+
+        private async void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            AppointmentDetail appointmentDetail = AppointmentDetailList.SelectedItem as AppointmentDetail;
+            if (appointmentDetail == null)
+            {
+                MessageBox.Show("AppointmentDetailID not found", "Warning");
+                return;
+            }
+
+            var result = await _appointmentDetailBusiness.DeleteById(appointmentDetail.AppointmentDetailId);
+            MessageBox.Show(result.Message, "Delete");
+            GetAllData();
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonSelect_Click(object sender, RoutedEventArgs e)
+        {
+            AppointmentDetail appointmentDetail = AppointmentDetailList.SelectedItem as AppointmentDetail;
+            AppointmentDetailId.Text = appointmentDetail.AppointmentDetailId.ToString();
+            AppointmentId.Text = appointmentDetail.AppointmentId.ToString();
+            ServiceId.Text = appointmentDetail.ServiceId.ToString();
+            isPeriodic.Text = appointmentDetail.IsPeriodic.ToString();
+            Day.Text = appointmentDetail.Day.ToString();
+            Month.Text = appointmentDetail.Month.ToString();
+            Year.Text = appointmentDetail.Year.ToString();
+        }
+
+        private async void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var appointmentDetailUpdate = new AppointmentDetail()
+                {
+                    AppointmentDetailId = Int32.Parse(AppointmentDetailId.Text),
+                    AppointmentId = Int32.Parse(AppointmentId.Text),
+                    ServiceId = Int32.Parse(ServiceId.Text),
+                    IsPeriodic = Boolean.Parse(isPeriodic.Text),
+                    Day = Int32.Parse(Day.Text),
+                    Month = Int32.Parse(Month.Text),
+                    Year = Int32.Parse(Year.Text),
+                };
+
+                var existingAppointmentDetail = await _appointmentDetailBusiness.GetById(appointmentDetailUpdate.AppointmentDetailId);
+                var appointmentDetail = existingAppointmentDetail.Data as AppointmentDetail;
+                if (existingAppointmentDetail.Data == null)
+                {
+                    System.Windows.MessageBox.Show("Appointemnt Detail ID doesn't exist", "Warning");
+                    return;
+                }
+                var temp = await _appointmentDetailBusiness.GetById(appointmentDetailUpdate.AppointmentId);
+                if (temp.Data == null)
+                {
+                    System.Windows.MessageBox.Show("Appointment ID doesn't exist", "Warning");
+                    return;
+                }
+                temp = await _appointmentDetailBusiness.GetById(appointmentDetailUpdate.ServiceId);
+                if (temp.Data == null)
+                {
+                    System.Windows.MessageBox.Show("Service ID doesn't exist", "Warning");
+                    return;
+                }
+
+                temp = await _appointmentDetailBusiness.GetById(appointmentDetailUpdate.AppointmentDetailId);
+                if (temp.Data != null)
+                {
+                    System.Windows.MessageBox.Show("Appointment ID doesn't exist\", \"Warning");
+                    return;
+                }
+
+                //update
+                appointmentDetail.AppointmentDetailId = appointmentDetailUpdate.AppointmentDetailId;
+                appointmentDetail.AppointmentId = appointmentDetailUpdate.AppointmentId;
+                appointmentDetail.ServiceId = appointmentDetailUpdate.ServiceId;
+                appointmentDetail.IsPeriodic = appointmentDetailUpdate.IsPeriodic;
+                appointmentDetail.Day = appointmentDetailUpdate.Day;
+                appointmentDetail.Month = appointmentDetailUpdate.Month;
+                appointmentDetail.Year = appointmentDetailUpdate.Year;
+
+                var result = await _appointmentDetailBusiness.Update(appointmentDetail);
+                System.Windows.MessageBox.Show(result.Message, "Update");
+
+                //reset text box
+                AppointmentDetailId.Text = string.Empty;
+                AppointmentId.Text = string.Empty;
+                ServiceId.Text = string.Empty;
+                isPeriodic.Text = string.Empty;
+                Day.Text = string.Empty;
+                Month.Text = string.Empty;
+                Year.Text = string.Empty;
+                GetAllData();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show($"{ex.Message}", "Error");
+            }
+        }
+
+        private async void GetAllData()
+        {
+            var result = await _appointmentDetailBusiness.GetAll();
+
+            if (result.Status > 0 && result.Data != null)
+            {
+                AppointmentDetailList.ItemsSource = result.Data as List<AppointmentDetail>;
+            }
+            else
+            {
+                AppointmentDetailList.ItemsSource = new List<AppointmentDetail>();
+            }
+        }
+    }
+}

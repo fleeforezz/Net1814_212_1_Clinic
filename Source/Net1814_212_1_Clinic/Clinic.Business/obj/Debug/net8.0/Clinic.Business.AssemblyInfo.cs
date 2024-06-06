b418c18dd07a091dab673cf259d581a5781ffc1e
Trương Minh Nhật <fleeforezz@gmail.com>
2024-06-06 18:40:42 +0700

    Update UI

--- Source/Net1814_212_1_Clinic/Clinic.Business/obj/Debug/net8.0/Clinic.Business.AssemblyInfo.cs
+++ Source/Net1814_212_1_Clinic/Clinic.Business/obj/Debug/net8.0/Clinic.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clinic.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485f576c18b4e740dbc669378c0e62a22603dfbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clinic.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clinic.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Source/Net1814_212_1_Clinic/Clinic.Data/obj/Debug/net8.0/Clinic.Data.AssemblyInfo.cs
+++ Source/Net1814_212_1_Clinic/Clinic.Data/obj/Debug/net8.0/Clinic.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clinic.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485f576c18b4e740dbc669378c0e62a22603dfbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clinic.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clinic.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

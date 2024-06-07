f2157f426d6eae7265e3a660f197604f17df1794
Trương Minh Nhật <fleeforezz@gmail.com>
2024-06-07 23:21:07 +0700

    Update

--- Source/Net1814_212_1_Clinic/Clinic.Common/obj/Debug/net8.0/Clinic.Common.AssemblyInfo.cs
+++ Source/Net1814_212_1_Clinic/Clinic.Common/obj/Debug/net8.0/Clinic.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clinic.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485f576c18b4e740dbc669378c0e62a22603dfbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76698dffe2627c9e456b3fb33200aa152b26bfc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clinic.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clinic.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
